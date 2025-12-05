2117977410bb9e740aee20ef0d41a7f23ab2aac2
patrik <smrzo@ASUS_PATRIK>
2025-12-05 15:49:06 +0100

    s

--- TriCore OS/Grafika/loginScreenUI.cs
+++ TriCore OS/Grafika/loginScreenUI.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.Marshalling;
 using System.Text;
-using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace TriCore_OS.Grafika
 {
-    internal class logoSchreen
+    internal class loginScreenUI
     {
-        
+        public void LoginScreenUI()
+        {
+          
+        }
     }
 }
