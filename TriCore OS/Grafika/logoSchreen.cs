4890ef6585f29e3dcd6ec22fc36df28e7fac237b
patrik <smrzo@ASUS_PATRIK>
2025-12-05 16:24:01 +0100

    added start UI

--- TriCore OS/Grafika/logoSchreen.cs
+++ TriCore OS/Grafika/logoSchreen.cs
@@ -10,6 +10,24 @@ namespace TriCore_OS.Grafika
 {
     internal class logoSchreen
     {
-        
+        public void StartUI()
+        {
+            string logoScreen = @"
+   .----.  .----.  .----.   .----.  .----.  .----.
+  /  /\  \/  /\  \/  /\  \ /  /\  \/  /\  \/  /\  \
+ /__/_/___\_/__\_/__/_/___\_/__/_/___\_/__\_/__/_/__\
+ |  TRI  |  CORE  |   O S   |   v2.5   |  LOADING   |
+ \  \/  /\/  \/  /\/  \/  /\/  \/  /\/  \/  /\/  \/  /
+  '----'  '----'  '----'   '----'  '----'  '----'
+";
+            Console.Clear();
+            Console.Write(logoScreen);
+
+        }
+
+        public void LoadingScreen()
+        {
+
+        }
     }
 }
--- TriCore OS/Program.cs
+++ TriCore OS/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("TriCore OS");
+﻿
+using TriCore_OS.Grafika;
+
+Console.WriteLine("TriCore OS");
+
+logoSchreen logo = new logoSchreen();
+logo.StartUI();
