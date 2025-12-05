9f4fe5126570b616013eac6d328c63fc9659d416
patrik <smrzo@ASUS_PATRIK>
2025-12-05 17:25:26 +0100

    Added logo sceen with center

--- TriCore OS/Grafika/logoSchreen.cs
+++ TriCore OS/Grafika/logoSchreen.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.Marshalling;
 using System.Text;
 using System.Threading.Channels;
@@ -10,9 +11,7 @@ namespace TriCore_OS.Grafika
 {
     internal class logoSchreen
     {
-        public void StartUI()
-        {
-            string logoScreen = @"
+       private string logoScreen = @"
    .----.  .----.  .----.   .----.  .----.  .----.
   /  /\  \/  /\  \/  /\  \ /  /\  \/  /\  \/  /\  \
  /__/_/___\_/__\_/__/_/___\_/__/_/___\_/__\_/__/_/__\
@@ -20,14 +19,90 @@ namespace TriCore_OS.Grafika
  \  \/  /\/  \/  /\/  \/  /\/  \/  /\/  \/  /\/  \/  /
   '----'  '----'  '----'   '----'  '----'  '----'
 ";
-            Console.Clear();
+        private string PositionUI()
+        {
+              int withConsole = Console.WindowWidth;
+             int heightConsole = Console.WindowHeight;
+            int xPos = 3;
+            int yPos = 10;
+            Console.SetCursorPosition(xPos, yPos);
+            return PositionUI();
+        }
+      
+
+        private void LogoUI()
+        {
+           
             Console.Write(logoScreen);
 
         }
 
-        public void LoadingScreen()
-        {
 
+
+        private void LoadingUI()
+        {
+            PositionUI();
+            LogoUI();
+            Thread.Sleep(500);
+            Console.WriteLine("[░░░░░░░░░░░░░░░░]   0%  ─⊙─  Starting boot sequence");
+            Thread.Sleep(500);
+            Console.Clear();
+            PositionUI(); 
+            LogoUI();
+            Console.WriteLine(" [██░░░░░░░░░░░░░░]  10%  ─⊙─  Initializing I/O");
+            Thread.Sleep(700);
+            Console.Clear();
+            PositionUI();
+            LogoUI();
+            Console.WriteLine(" [████░░░░░░░░░░░░]  20%  ─⊙─  Loading core drivers");
+            Thread.Sleep(400);
+            Console.Clear();
+            PositionUI();
+            LogoUI();
+            Console.WriteLine("[██████░░░░░░░░░░]  30%  ─⊙─  Detecting hardware");
+            Thread.Sleep(400);
+            Console.Clear();
+            PositionUI();
+            LogoUI();
+            Console.WriteLine(" [████████░░░░░░░░]  40%  ─⊙─  Mounting virtual FS");
+            Thread.Sleep(400);
+            Console.Clear();
+            PositionUI();
+            LogoUI();
+            Console.WriteLine(" [██████████░░░░░░]  50%  ─⊙─  Launching services");
+            Thread.Sleep(700);
+            Console.Clear();
+            PositionUI();
+            LogoUI();
+            Console.WriteLine("  [████████████░░░░]  60%  ─⊙─  Network stack online");
+            Thread.Sleep(100);
+            Console.Clear();
+            PositionUI();
+            LogoUI();
+            Console.WriteLine(" [██████████████░░]  70%  ─⊙─  Security modules ready");
+            Thread.Sleep(300);
+            Console.Clear();
+            PositionUI();
+            LogoUI();
+            Console.WriteLine(" [████████████████]  80%  ─⊙─  Optimizing system");
+            Thread.Sleep(300);
+            Console.Clear();
+            PositionUI();
+            LogoUI();
+            Console.WriteLine(" [██████████████████░░]  90% ─⊙─  Preparing UI");
+            Thread.Sleep(800);
+            Console.Clear();
+            PositionUI();
+            LogoUI();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(" [████████████████████] 100% ─⊙─  TriCore ready!");
+            Thread.Sleep(2000);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+        }
+        public void StartUI()
+        {
+            LoadingUI();
         }
     }
 }
