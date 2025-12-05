07c6a9b301db8793f422f20a695bd118cb589fe4
patrik <smrzo@ASUS_PATRIK>
2025-12-05 21:26:51 +0100

    bug fixes from merge

--- TriCore OS/Program.cs
+++ TriCore OS/Program.cs
@@ -1,7 +1,11 @@
 ﻿
+using TriCore_OS;
 using TriCore_OS.Grafika;
 
 Console.WriteLine("TriCore OS");
+
+FileEngine fileengine = new FileEngine();
+fileengine.EngineStart();
 
 //logoSchreen logo = new logoSchreen();
 //logo.StartUI();
