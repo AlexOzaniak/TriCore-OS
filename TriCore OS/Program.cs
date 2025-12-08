4985de8d936ff9c69448c54c5a5c05b167eae815
patrik <smrzo@ASUS_PATRIK>
2025-12-08 13:49:26 +0100

    repaired bugs

--- TriCore OS/Program.cs
+++ TriCore OS/Program.cs
@@ -2,11 +2,22 @@
 using TriCore_OS;
 using TriCore_OS.Grafika;
 
+
 Console.WriteLine("TriCore OS");
 
-//FileEngine fileengine = new FileEngine();
-//fileengine.EngineStart();
+
+int maxHeight = Console.LargestWindowHeight;
+int maxWith = Console.LargestWindowWidth;
+
+Console.SetBufferSize(maxWith, maxHeight);
+
+Console.WriteLine("Pre pokracovanie stlac enter");
+Console.ReadLine();
+Console.Clear();
 
 logoSchreen logo = new logoSchreen();
+
 logo.StartUI();
+Console.ReadKey();
+
 
