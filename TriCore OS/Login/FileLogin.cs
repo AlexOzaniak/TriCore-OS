18328840b947ecf8c024562d15883c4782ea9532
Alex Ozaniak <alexozaniakk@gmail.com>
2025-12-08 13:32:24 +0100

    bug fixed ( stale nefunkcny zapis do suboru()

--- TriCore OS/Login/FileLogin.cs
+++ TriCore OS/Login/FileLogin.cs
@@ -1,32 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace TriCore_OS.Login
 {
     public class FileLogin
     {
-         
-        protected string[] Username { get;  set; }
-        protected string[] Password { get;  set; }
+        protected string Username { get; set; }
+        protected string Password { get; set; }
+        public string Path { get; set; }
 
-        public string Path {  get; set; }
-
-
-
+       
 
         public void SaveDetails()
         {
-            File.WriteAllLines(new string (Path), new string []
-                {
-                Username[0],
-                Password[0]});
-
+            using (StreamWriter file = new StreamWriter(Path, true))
+            {
+                Console.WriteLine($"{Username};{Password}");
+            }
 
         }
-        
     }
 }
-
--- TriCore OS/Program.cs
+++ TriCore OS/Program.cs
@@ -1,5 +1,10 @@
-﻿using TriCore_OS;
+﻿using TriCore_OS.Grafika;
+using TriCore_OS.Login;
 
 Console.WriteLine("TriCore OS - start point");
 FileEngine engine = new FileEngine();
-engine.EngineStart();
+LoginDetailsExtraction login = new LoginDetailsExtraction();
+loginScreenUI ui = new loginScreenUI();
+ui.ui
+engine.EngineStart(login);
+
