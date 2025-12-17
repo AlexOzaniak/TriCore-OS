5e896f8813d3fef7598987cd8bbe456c4f02efaf
patrik <smrzo@ASUS_PATRIK>
2025-12-17 10:34:29 +0100

    mid save

--- TriCore OS/Program.cs
+++ TriCore OS/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using TriCore_OS.Grafika;
+
+Console.WriteLine("TriCore OS");
+Console.ReadLine();
+Console.Clear();
+
+
+
+logoSchreen logo = new logoSchreen();
+logo.StartUI();
+Console.Clear();
+TextCentering textCenter = new TextCentering();
+loginScreenUI loginUI = new loginScreenUI();
+
+var loginLines = loginUI.BuildLoginScreen();
+textCenter.CenterText(loginLines, Console.WindowHeight / 2);
+
+Console.ReadKey();
+Console.Clear();
+
+MainGraphics homeUI = new MainGraphics();
+homeUI.mainGraphics();
+Console.ReadKey();
