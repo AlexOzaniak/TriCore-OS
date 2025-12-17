8da49e5e8ff6604fc94366b276eb8879f48c694e
patrik <smrzo@ASUS_PATRIK>
2025-12-17 10:41:49 +0100

    created new Main_Engine

---
+++ TriCore OS/Main_Engine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriCore_OS.Grafika;
+
+namespace TriCore_OS
+{
+    internal class Main_Engine
+    {
+        public void Engine()
+        {
+
+            Console.WriteLine("TriCore OS");
+            Console.ReadLine();
+            Console.Clear();
+
+
+
+            logoSchreen logo = new logoSchreen();
+            logo.StartUI();
+            Console.Clear();
+            TextCentering textCenter = new TextCentering();
+            loginScreenUI loginUI = new loginScreenUI();
+
+            var loginLines = loginUI.BuildLoginScreen();
+            textCenter.CenterText(loginLines, Console.WindowHeight / 2);
+
+            Console.ReadKey();
+            Console.Clear();
+
+            MainGraphics homeUI = new MainGraphics();
+            homeUI.mainGraphics();
+            Console.ReadKey();
+        }
+    }
+}
--- TriCore OS/Program.cs
+++ TriCore OS/Program.cs
@@ -1,23 +1,4 @@
-﻿using TriCore_OS.Grafika;
+﻿using TriCore_OS;
 
-Console.WriteLine("TriCore OS");
-Console.ReadLine();
-Console.Clear();
-
-
-
-logoSchreen logo = new logoSchreen();
-logo.StartUI();
-Console.Clear();
-TextCentering textCenter = new TextCentering();
-loginScreenUI loginUI = new loginScreenUI();
-
-var loginLines = loginUI.BuildLoginScreen();
-textCenter.CenterText(loginLines, Console.WindowHeight / 2);
-
-Console.ReadKey();
-Console.Clear();
-
-MainGraphics homeUI = new MainGraphics();
-homeUI.mainGraphics();
-Console.ReadKey();
+Main_Engine engine = new Main_Engine();
+engine.Engine();
