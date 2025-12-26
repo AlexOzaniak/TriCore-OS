f6b1114d9f53e4fb58a9f568c92679f9f241e020
patrik <smrzo@ASUS_PATRIK>
2025-12-26 20:43:12 +0100

    save

--- TriCore OS/Program.cs
+++ TriCore OS/Program.cs
@@ -1,4 +1,19 @@
 ﻿using TriCore_OS;
 using TriCore_OS.Grafika;
-Main_Engine engine = new Main_Engine();
-engine.Engine();
+using TriCore_OS.Login;
+using TriCore_OS.Spustanie_Programov;
+
+FileEngine engine = new FileEngine();
+loginScreenUI ui = new loginScreenUI();
+MainGraphics mainGraphics = new MainGraphics();
+Main_Engine mainEngine = new Main_Engine(); 
+
+TextCentering textCentering = new TextCentering();
+logoScreen ui2 = new logoScreen();
+LoginDetailsExtraction extraction = new LoginDetailsExtraction();
+Zobrazenie_Casu cas = new Zobrazenie_Casu();
+cas.ShowTime();
+Console.ReadKey();
+
+
+
