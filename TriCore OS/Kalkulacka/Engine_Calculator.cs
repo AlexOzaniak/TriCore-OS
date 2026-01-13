5cb7ecdeb413b5eab08161f1a1482bb5c2c2f124
patrik <smrzo@ASUS_PATRIK>
2026-01-13 23:21:08 +0100

    started making calculator

---
+++ TriCore OS/Kalkulacka/Engine_Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriCore_OS.Kalkulacka
+{
+    internal class Engine_Calculator
+    {
+        public void StartCalculator()
+        {
+            Console.WriteLine("Vitaj v TriCore Calculator!");
+            Thread.Sleep(500);
+            Console.WriteLine("Pre spustenie stlac 'ENTER'");
+            Console.ReadLine();
+            Console.Clear();
+
+
+        }
+    }
+}
