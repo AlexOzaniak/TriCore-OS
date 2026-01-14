4a40a92eda51cab823e777b5fe9a276bb60742e9
patrik <smrzo@ASUS_PATRIK>
2026-01-14 20:38:08 +0100

    conflict save

--- TriCore OS/Program.cs
+++ TriCore OS/Program.cs
@@ -1,44 +1,29 @@
 ﻿using TriCore_OS.BabetaMaster;
-
-Engine engine = new Engine();
-engine.Welcome();
-engine.Menu();
-
-engine.Loading();
-//﻿using TriCore_OS;
-//using TriCore_OS.Grafika;
-//using TriCore_OS.Login;
-//using TriCore_OS.Programs;
-//using TriCore_OS.Spustanie_Programov;
-
-
-//FileEngine engine = new FileEngine();
-//loginScreenUI ui = new loginScreenUI();
-//MainGraphics mainGraphics = new MainGraphics();
-//Main_Engine mainEngine = new Main_Engine();
-//mainEngine.Engine();
-
-
-
-//TextCentering textCentering = new TextCentering();
-//logoScreen ui2 = new logoScreen();
-//LoginDetailsExtraction extraction = new LoginDetailsExtraction();
-//Zobrazenie_Casu cas = new Zobrazenie_Casu();
-//StartingProgram program = new StartingProgram();
-//Login_Engine login_Engine = new Login_Engine();
-//FileLogin login = new FileLogin();
-//AppCommandList list = new AppCommandList();
-//MomsFood momsFood = new MomsFood();
+﻿using TriCore_OS;
+using TriCore_OS.Grafika;
+using TriCore_OS.Login;
+using TriCore_OS.Programs;
+using TriCore_OS.Spustanie_Programov;
+using TriCore_OS.Spustanie_Programov;
+FileEngine engine1 = new FileEngine();
+Engine enginegame = new Engine();
+loginScreenUI ui = new loginScreenUI();
+MainGraphics mainGraphics = new MainGraphics();
+Main_Engine mainEngine = new Main_Engine();
+logoScreen ui2 = new logoScreen();
+LoginDetailsExtraction extraction = new LoginDetailsExtraction();
+Zobrazenie_Casu cas = new Zobrazenie_Casu();
+StartingProgram program = new StartingProgram();
+Login_Engine login_Engine = new Login_Engine();
+FileLogin login = new FileLogin();
+AppCommandList list = new AppCommandList();
+MomsFood momsFood = new MomsFood();
+Zobrazenie_Casu zobrazenie_Casu = new Zobrazenie_Casu();
 
 
-/*
-for (int  i = 0; i < 1; i++)
-{
-    program.StartProgram();
-    
-    Console.ReadKey();
-}
-*/
+
+zobrazenie_Casu.ShowTime();
+
 
 
 //login_Engine.StartLoginProcess(engine,login);
